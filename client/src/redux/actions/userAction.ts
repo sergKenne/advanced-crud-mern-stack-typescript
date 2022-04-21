260ba00bebe71fac67dec58db016d59c51e2694c
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-04-21 18:37:23 +0300

    fix deploying

--- client/src/redux/actions/userAction.ts
+++ client/src/redux/actions/userAction.ts
@@ -24,17 +24,27 @@ export const fetchUsers = () => async(dispatch:Dispatch) => {
 
 export const addUser = (user: any) => async (dispatch: Dispatch) => {
     
-    //dispatch({ type: UserActionType.CREATE_USER_REQUEST });
+    dispatch({ type: UserActionType.CREATE_USER_REQUEST });
 
-    fetch('/api/user', {
-        method: 'POST',
-        body: user,
-    }).then((response) => {
-
-        console.log(response.json());
+    fetch('/api/user', {method: 'POST',body: user,})
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.user);
+            dispatch({
+                type: UserActionType.CREATE_USER_SUCCESS,
+                payload: data.user
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({
+                type: UserActionType.CREATE_USER_FAIL,
+                payload: err
+            })
+        })
+    
         
         
-    });
 
     // try {
     //     const { data } = await axios.post('/api/user', user);
