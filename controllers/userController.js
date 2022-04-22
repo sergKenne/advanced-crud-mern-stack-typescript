94356bd3b7cf22b146e103edd22c4a61215ab79a
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-04-22 04:20:51 +0300

    fix changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,11 +28,6 @@ const addUser = async (req, res) => {
         return res.status(422).json({ msg: 'please choose the image' });
     }
 
-    //res.setHeader('Access-Control-Allow-Origin', '*');
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    //res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-
     try {
         const user = new User({ email, image, name, phone });
         await user.save().then((result) => {
